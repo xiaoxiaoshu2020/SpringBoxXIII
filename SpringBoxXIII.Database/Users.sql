2f2fbe619c7ee87b24871a3a16efcd74f9aebb00
xiaoxiaoshu <xiaoxiaoshu2020@outlook.com>
2025-08-15 15:13:42 +0800

    feat:添加SQL项目，并设计User表

---
+++ SpringBoxXIII.Database/Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+    [UserId] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [UserName] NVARCHAR(50) NOT NULL,
+    [PasswordHash] VARBINARY(256) NOT NULL,
+    [PasswordSalt] VARBINARY(128) NOT NULL,
+    [Count] INT NOT NULL DEFAULT 0,
+    
+    CONSTRAINT [UK_Users_UserName] UNIQUE ([UserName]), 
+    CONSTRAINT [CK_Users_Count] CHECK ([Count]>=0) 
+);
